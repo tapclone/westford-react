4b759d439db7a10fc38e094e023808e1712292f5
Anandu A <anandua606@gmail.com>
2023-02-11 18:27:14 +0530

    design corrections

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import banner from '../../src/images/about-us/about-hero-image.png'
 import bannerOverLay from '../../src/images/westford assets/overlay.svg'
 import heroImg from '../../src/images/about-us/abt-westford-lady.png'
@@ -9,9 +9,58 @@ import person4 from '../../src/images/about-us/firoz.png'
 import person5 from '../../src/images/about-us/ahluwalia.png'
 import person6 from '../../src/images/about-us/ali.png'
 import zigzag from '../../src/images/about-us/zig-zag.png'
+import trophy from '../../src/images/about-us/trophy.png'
+import Slider from 'react-slick'
+import upBtn from '../../src/images/about-us/Group 831.svg'
+import downBtn from '../../src/images/about-us/Group 832.svg'
+import btnBar from '../../src/images/about-us/Group 833.svg'
 
 
 function About() {
+    function CustomArrowR(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick}
+            >
+                
+                <img className="slideBtn" style={{ width: '2rem' }} src={downBtn} alt="" />
+            </div>
+        );
+    }
+    function CustomArrowL(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick}
+            >
+                
+                <img className="slideBtn" style={{ width: '2rem' }} src={upBtn} alt="" />
+            </div>
+        );
+    }
+
+    const [currentSlide,setCurrentSlide]=useState(0)
+    const settings4 = {
+        infinite: true,
+        speed: 800,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        vertical: true,
+        nextArrow: <CustomArrowR></CustomArrowR>,
+        prevArrow: <CustomArrowL></CustomArrowL>,
+        beforeChange: (current, next) => {
+            setCurrentSlide(current);
+          },
+          afterChange: (index) => {
+            setCurrentSlide(index);
+          }
+      };
+      const arr=[1,2,3,4,5,6]
     return (
         <div style={{ width: '100%', overflowX: 'hidden' }}>
             <div class="hero" style={{ backgroundImage: `url(${banner}),url(${bannerOverLay})`, backgroundPosition: "center center", backgroundSize: 'cover', backgroundRepeat: "no-repeat", backgroundBlendMode: 'multiply' }}>
@@ -41,9 +90,9 @@ function About() {
                     <div className='startingContentText' style={{ width: '60%', display: 'flex', alignItems: 'start', justifyContent: 'end' }}>
                         <div style={{ width: '93%' }}>
                             <h2 style={{ marginTop: "0", fontSize: "clamp(1.5rem,3vw,3rem)", color: ' #303030', fontWeight: "600", marginBottom: '10px' }}>About Westford</h2>
-                            <p style={{ textAlign: 'justify', fontSize: '1rem', lineHeight: '23px', marginBottom: "0" }}>Westford Education Group (WEG) operating as Westford Ventures Holding Ltd., A ADGM registered company with multiple brands of higher education programs through its campuses and satellite learning centres all across the globe. The group is established with an objective to fulfil the aspirations of learners with diverse backgrounds from across the globe to attain internationally-accredited qualifications in the form of on-site and online learning interventions.</p>
-                            <p style={{ textAlign: 'justify', lineHeight: '23px', fontSize: '1rem', marginBottom: '0' }}>Working toward a world where everyone has access to higher education, WEG now stands out winning the confidence of its learners and the community, a result of its steadfast commitment to students’ welfare and their success stories. </p>
-                            <p style={{ textAlign: 'justify', lineHeight: '23px', fontSize: '1rem', marginBottom: "0" }}> At Westford, we have come up with an integrated strategy and operational model for the global education. The group’s overall mission is “enabling the student of today to become a successful individual of tomorrow”.</p>
+                            <p style={{ textAlign: 'justify', fontSize: '0.9rem', lineHeight: '23px', marginBottom: "0" }}>Westford Education Group (WEG) operating as Westford Ventures Holding Ltd., A ADGM registered company with multiple brands of higher education programs through its campuses and satellite learning centres all across the globe. The group is established with an objective to fulfil the aspirations of learners with diverse backgrounds from across the globe to attain internationally-accredited qualifications in the form of on-site and online learning interventions.</p>
+                            <p style={{ textAlign: 'justify', lineHeight: '23px', fontSize: '0.9rem', marginBottom: '0' }}>Working toward a world where everyone has access to higher education, WEG now stands out winning the confidence of its learners and the community, a result of its steadfast commitment to students’ welfare and their success stories. </p>
+                            <p style={{ textAlign: 'justify', lineHeight: '23px', fontSize: '0.9rem', marginBottom: "0" }}> At Westford, we have come up with an integrated strategy and operational model for the global education. The group’s overall mission is “enabling the student of today to become a successful individual of tomorrow”.</p>
                         </div>
 
                     </div>
@@ -51,200 +100,37 @@ function About() {
                 </div>
 
             </div>
+            
+            <section className='mileStoneSlider' style={{display:'flex',alignItems:'center',justifyContent:'center',marginTop:"5%",background:'#F8F8F8',padding:"5% 0",position:'relative'}}>
+                {/* <img src={btnBar} style={{position:'absolute',top:'28',right:'12.7'}} alt="" /> */}
+                <div style={{width:"65%"}}>
+                    <Slider {...settings4}>
+                        {arr.map((item,index)=>{
+                           return <div>
 
-
-            {/* <div class="about-milestone">
-
-        <div class="milestone-heading">
-            <h2>Milestones</h2>
-        </div>
-        <div class="main-milestone slide display">
-                    <div class="main-milestone-left">
-                        <h3>2009</h3>
-
-                    </div>
-
-                    <div class="main-milestone-right">
-                        <div class="right-all-contents">
-                            <div class="right-all-row1">
-                                <img src="/westfordeducation/images/about-us/trophy-icon.png" alt="" />
-                                <p>01</p>
+                            <div style={{transform: `scale(${index === currentSlide ? 1 : 0.7})`}}  className='milStoneSliderSingle'>
+                                <div className='overlaySmallSlider' style={{background:`${index!=currentSlide?'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 0.01%, #F8F8F8 110%)':'transparent'}`}}></div>
+                                <div className='milStoneSliderSingleSub1' style={{width:'30%',background:'#DB3656',display:'flex',alignItems:'center',justifyContent:'center'}}>
+                                    <h2 style={{fontSize:'2.3rem',color:'white'}}>2000</h2>
+                                </div>
+                                <div className='milStoneSliderSingleSub2' style={{width:'70%',display:"flex",alignItems:'center',justifyContent:'center'}}>
+                                    <div >
+                                        <img src={trophy} alt="" />
+                                        <span style={{fontSize:'2rem',color:'#303030',opacity:"0.3"}}>0{item}</span>
+                                        <p>Rechristened westford school of management as westford university college and expanded its portfolio of partners. Programs and qualification</p>
+                                    </div>
+                                </div>
                             </div>
-
-                            <div class="right-all-row2">
-                                <p>Rechristened westford school of management as westford <br />
-                                    university college and expanded its portfolio of partners. <br />
-                                    Programs and qualification </p>
                             </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-        <div style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <div class="mileStoneSlider">
-                
-
-                <div class="main-milestone slide">
-                    <div class="main-milestone-left">
-                        <h3>2010</h3>
-
-                    </div>
-
-                    <div class="main-milestone-right">
-                        <div class="right-all-contents">
-                            <div class="right-all-row1">
-                                <img src="/westfordeducation/images/about-us/trophy-icon.png" alt="" />
-                                <p>01</p>
-                            </div>
-
-                            <div class="right-all-row2">
-                                <p>Rechristened westford school of management as westford <br />
-                                    university college and expanded its portfolio of partners. <br />
-                                    Programs and qualification </p>
-                            </div>
-
-
-                        </div>
-
-                    </div>
+                        })}
+                    
+                    
+                    </Slider>
+                    
+                    
 
                 </div>
-
-                <div class="main-milestone slide">
-                    <div class="main-milestone-left">
-                        <h3>2011</h3>
-
-                    </div>
-
-                    <div class="main-milestone-right">
-                        <div class="right-all-contents">
-                            <div class="right-all-row1">
-                                <img src="/westfordeducation/images/about-us/trophy-icon.png" alt="" />
-                                <p>01</p>
-                            </div>
-
-                            <div class="right-all-row2">
-                                <p>Rechristened westford school of management as westford <br />
-                                    university college and expanded its portfolio of partners. <br />
-                                    Programs and qualification </p>
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="main-milestone slide">
-                    <div class="main-milestone-left">
-                        <h3>2012</h3>
-
-                    </div>
-
-                    <div class="main-milestone-right">
-                        <div class="right-all-contents">
-                            <div class="right-all-row1">
-                                <img src="/westfordeducation/images/about-us/trophy-icon.png" alt="" />
-                                <p>01</p>
-                            </div>
-
-                            <div class="right-all-row2">
-                                <p>Rechristened westford school of management as westford <br />
-                                    university college and expanded its portfolio of partners. <br />
-                                    Programs and qualification </p>
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div style={{display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',rowGap:'1rem'}}>
-                <button id="prev">Previous</button>
-                <button id="next">Next</button>
-            </div>
-        </div>
-
-
-         <div class="old-milestone">
-            <div class="old-milestone-left">
-                <h3>2010</h3>
-
-            </div>
-
-            <div class="old-milestone-right">
-                <div class="right-all-contents-old">
-                    <div class="right-all-row1-old">
-                        <img src="/westfordeducation/images/about-us/trophy-icon.png" alt="" />
-                        <p>01</p>
-                    </div>
-
-                    <div class="right-all-row2-old">
-                        <p>Rechristened westford school of management as westford <br />
-                            university college and expanded its portfolio of partners. <br />
-                            Programs and qualification </p>
-                    </div>
-
-
-                </div>
-
-            </div>
-
-        </div> 
-
-    </div> */}
-
-            {/* <div class="vision-ethos">
-        <div class="vm-both" style={{width:"100%"}}>
-            <div class="vision-mision-left">
-                <h3>Our Vision & Mission</h3>
-                <div class="pointer-row">
-                    <img src="/westfordeducation/images/about-us/arrow-pointer-to-right.png" alt="" />
-                    <p>To be an unwavering flag-bearer in providing accredited qualifications delivered in a flexible,
-                        accessible, and affordable manner to the largest international network of learners.</p>
-                </div>
-
-                <div class="pointer-row">
-                    <img src="/westfordeducation/images/about-us/arrow-pointer-to-right.png" alt="" />
-                    <p>To ensure access to quality higher education for learners across the globe.</p>
-                </div>
-
-                <div class="pointer-row">
-                    <img src="/westfordeducation/images/about-us/arrow-pointer-to-right.png" alt="" />
-                    <p>To foster the development of talent while simultaneously empowering the modern workforce through
-                        continued education </p>
-                </div>
-
-                <div class="pointer-row">
-                    <img src="/westfordeducation/images/about-us/arrow-pointer-to-right.png" alt="" />
-                    <p>To pave the way for a better world by transforming the lives of individuals.</p>
-                </div>
-
-            </div>
-            <div class="ethos-right">
-                <h3>Ethos</h3>
-                <p>The focal point of all our endeavours is the ‘student experience’. Today, our world-class education
-                    has reached over 20,000 students. Our students form a vibrant, diverse and inclusive community of
-                    learners, contributing invaluable knowledge, innovative ideas, and distinctly fresh perspectives
-                    which propel their professional and personal journeys forward. Our faculty team comprises eminent
-                    professionals and academicians from reputed universities possessing a wealth of teaching and
-                    training experience. This medley of expertise in the industry and academia help them bring
-                    real-world experiences to the class, thereby anchoring our courses in a practical and
-                    work-world-based approach.</p>
-
-            </div>
-        </div>
-
-    </div> */}
+            </section>
             <section className='visionMission' style={{ display: 'flex', columnGap: '1rem', justifyContent: 'center',marginTop:'5rem',marginBottom:'3rem'}}>
                 <div style={{ width: '45%', height: 'inherit', background: '#ecf1f4', padding: '2%' }}>
                 <h1 style={{ fontSize: "clamp(1.5rem,3vw,3rem)",color: ' #303030', fontWeight: "600", margin: '0' }}>Our Vision & Mission </h1>
@@ -274,7 +160,7 @@ function About() {
 
 
 
-            <div class="our-leaders" style={{ height: 'fit-content' }}>
+            <div class="our-leaders" style={{ height: 'fit-content' ,paddingBottom:'15%'}}>
                 <div class="leadership-headings" style={{width:'60%'}}>
                     <h3 style={{fontSize: "clamp(1.5rem,3vw,3rem)",color:'#303030',marginBottom:'0'}}>Our Leadership</h3>
                     <p style={{ width: '100%',marginBottom:'3.5rem' }}>Our team of eminent professionals from the academia and industry, possessing decades of experience and
@@ -286,7 +172,7 @@ function About() {
                 <div class="leaders-pic" style={{ width: '90%', height: 'fit-content' }}>
 
                     <div class="leaders-row">
-                        <div class="leaders-img-text">
+                        <div class="leaders-img-text" >
                             <img src={person1} alt="" />
                             <div class="leaders-indiv-bottom">
                                 <h3>Firoz Thairinil</h3>
@@ -295,8 +181,8 @@ function About() {
 
                         </div>
 
-                        <div class="leaders-img-text">
-                            <img src={person2} alt="" />
+                        <div class="leaders-img-text" >
+                            <img  src={person2} alt="" />
                             <div class="leaders-indiv-bottom">
                                 <h3>Samras Mayimi</h3>
                                 <p>Co-founder and Executive Director</p>
--- frontend/src/Components/ArticleSection.js
+++ frontend/src/Components/ArticleSection.js
@@ -1,49 +1,101 @@
 import React from 'react'
+import Slider from 'react-slick'
 import img from '../../src/images/home/art-sect-img1.png'
 
 function ArticleSection() {
-  return (
-    <section className='articlesSection'>
-        
+    const settings3 = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        // autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false,
+                    // autoplay: true,
+                    autoplaySpeed: 1500,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    // autoplay: true,
+                    autoplaySpeed: 1500,
+                }
+            }
+        ]
+    };
+    return (
+        <div style={{ width: '100%', display: "flex", alignItems: 'center', justifyContent: 'center',marginBottom:'5%'}}>
+            <div style={{width:'86%'}}>
+                <Slider {...settings3}>
+                    <div>
+                        <div className='articleCard' style={{ background: `url(${img})`, backgroundBlendMode: "multiply" }}>
+                            <span style={{ background: 'white', padding: '7px', borderRadius: '5px' }}>MARKETING</span>
+                            <p className='articleCardChild1' style={{ color: 'white', padding: '0 2%',fontSize: '1.2rem', fontWeight: '500' }}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p className='articleCardChild2' style={{ color: 'white', padding: '0 2%' }}>Nov 14  .  10 min Read</p>
+                        </div>
+                    </div>
 
-        <div className='articleCard' style={{background:`url(${img})`,backgroundBlendMode:"multiply"}}>
-            <span style={{background:'white',padding:'7px',borderRadius:'5px',position:'absolute',top:'4%',left:'5%',}}>MARKETING</span> 
-            <p style={{color:'white',position:'absolute',bottom:'10%',padding:'0 2%',fontSize:'1.2rem',fontWeight:'500'}}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> 
-            <p style={{color:'white',position:'absolute',bottom:'1%',padding:'0 2%'}}>Nov 14  .  10 min Read</p>  
+                    <div>
+                        <div className='articleCard' style={{ background: `url(${img})`, backgroundBlendMode: "multiply" }}>
+                            <span style={{ background: 'white', padding: '7px', borderRadius: '5px' }}>MARKETING</span>
+                            <p className='articleCardChild1' style={{ color: 'white', padding: '0 2%',fontSize: '1.2rem', fontWeight: '500' }}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p className='articleCardChild2' style={{ color: 'white', padding: '0 2%' }}>Nov 14  .  10 min Read</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className='articleCard' style={{ background: `url(${img})`, backgroundBlendMode: "multiply" }}>
+                            <span style={{ background: 'white', padding: '7px', borderRadius: '5px' }}>MARKETING</span>
+                            <p className='articleCardChild1' style={{ color: 'white', padding: '0 2%',fontSize: '1.2rem', fontWeight: '500' }}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p className='articleCardChild2' style={{ color: 'white', padding: '0 2%' }}>Nov 14  .  10 min Read</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className='articleCard' style={{ background: `url(${img})`, backgroundBlendMode: "multiply" }}>
+                            <span style={{ background: 'white', padding: '7px', borderRadius: '5px' }}>MARKETING</span>
+                            <p className='articleCardChild1' style={{ color: 'white', padding: '0 2%',fontSize: '1.2rem', fontWeight: '500' }}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p className='articleCardChild2' style={{ color: 'white', padding: '0 2%' }}>Nov 14  .  10 min Read</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className='articleCard' style={{ background: `url(${img})`, backgroundBlendMode: "multiply" }}>
+                            <span style={{ background: 'white', padding: '7px', borderRadius: '5px' }}>MARKETING</span>
+                            <p className='articleCardChild1' style={{ color: 'white', padding: '0 2%',fontSize: '1.2rem', fontWeight: '500' }}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p className='articleCardChild2' style={{ color: 'white', padding: '0 2%' }}>Nov 14  .  10 min Read</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className='articleCard' style={{ background: `url(${img})`, backgroundBlendMode: "multiply" }}>
+                            <span style={{ background: 'white', padding: '7px', borderRadius: '5px' }}>MARKETING</span>
+                            <p className='articleCardChild1' style={{ color: 'white', padding: '0 2%',fontSize: '1.2rem', fontWeight: '500' }}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                            <p className='articleCardChild2' style={{ color: 'white', padding: '0 2%' }}>Nov 14  .  10 min Read</p>
+                        </div>
+                    </div>
+
+                    
+
+
+
+                </Slider>
+            </div>
         </div>
 
-        <div className='articleCard' style={{background:`url(${img})`,backgroundBlendMode:"multiply"}}>
-            <span style={{background:'white',padding:'7px',borderRadius:'5px',position:'absolute',top:'4%',left:'5%',}}>MARKETING</span> 
-            <p style={{color:'white',position:'absolute',bottom:'10%',padding:'0 2%',fontSize:'1.2rem',fontWeight:'500'}}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> 
-            <p style={{color:'white',position:'absolute',bottom:'1%',padding:'0 2%'}}>Nov 14  .  10 min Read</p>  
-        </div>
-
-        <div className='articleCard' style={{background:`url(${img})`,backgroundBlendMode:"multiply"}}>
-            <span style={{background:'white',padding:'7px',borderRadius:'5px',position:'absolute',top:'4%',left:'5%',}}>MARKETING</span> 
-            <p style={{color:'white',position:'absolute',bottom:'10%',padding:'0 2%',fontSize:'1.2rem',fontWeight:'500'}}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> 
-            <p style={{color:'white',position:'absolute',bottom:'1%',padding:'0 2%'}}>Nov 14  .  10 min Read</p>  
-        </div>
-
-        <div className='articleCard' style={{background:`url(${img})`,backgroundBlendMode:"multiply"}}>
-            <span style={{background:'white',padding:'7px',borderRadius:'5px',position:'absolute',top:'4%',left:'5%',}}>MARKETING</span> 
-            <p style={{color:'white',position:'absolute',bottom:'10%',padding:'0 2%',fontSize:'1.2rem',fontWeight:'500'}}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> 
-            <p style={{color:'white',position:'absolute',bottom:'1%',padding:'0 2%'}}>Nov 14  .  10 min Read</p>  
-        </div>
-
-        <div className='articleCard' style={{background:`url(${img})`,backgroundBlendMode:"multiply"}}>
-            <span style={{background:'white',padding:'7px',borderRadius:'5px',position:'absolute',top:'4%',left:'5%',}}>MARKETING</span> 
-            <p style={{color:'white',position:'absolute',bottom:'10%',padding:'0 2%',fontSize:'1.2rem',fontWeight:'500'}}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> 
-            <p style={{color:'white',position:'absolute',bottom:'1%',padding:'0 2%'}}>Nov 14  .  10 min Read</p>  
-        </div>
-
-        <div className='articleCard' style={{background:`url(${img})`,backgroundBlendMode:"multiply"}}>
-            <span style={{background:'white',padding:'7px',borderRadius:'5px',position:'absolute',top:'4%',left:'5%',}}>MARKETING</span> 
-            <p style={{color:'white',position:'absolute',bottom:'10%',padding:'0 2%',fontSize:'1.2rem',fontWeight:'500'}}> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> 
-            <p style={{color:'white',position:'absolute',bottom:'1%',padding:'0 2%'}}>Nov 14  .  10 min Read</p>  
-        </div>
-
-    </section>
-  )
+    )
 }
 
 export default ArticleSection
