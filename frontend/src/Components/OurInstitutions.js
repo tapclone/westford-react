071801a4f5fbac3139ab3d98b90d789d34c61215
Anandu A <anandua606@gmail.com>
2023-02-14 12:21:46 +0530

    design bug

--- frontend/src/Components/OurInstitutions.js
+++ frontend/src/Components/OurInstitutions.js
@@ -1,12 +1,13 @@
+
 import React, { useEffect, useState } from "react";
-import bannerOverLay from "../../src/images/westford assets/overlay.svg";
-import banner from "../../src/images/our-institutions/hero-bg.png";
-import westford from "../../src/images/home/westford-logo.png";
-import heroImg from "../../src/images/about-us/our-institutions-lady.png";
-import zigzag from "../../src/images/about-us/zig-zag.png";
-import zigzagBlue from "../../src/images/about-us/zigzag-blue.png";
-import "./About.css";
-import ArticleSection from "./ArticleSection";
+import bannerOverLay from '../../src/images/westford assets/overlay.svg'
+import banner from '../../src/images/our-institutions/hero-bg.png'
+import westford from '../../src/images/home/westford-logo.png'
+import heroImg from '../../src/images/about-us/our-institutions-lady.png'
+import zigzag from '../../src/images/about-us/zig-zag.png'
+import zigzagBlue from '../../src/images/about-us/zigzag-blue.png'
+import './About.css'
+import ArticleSection from './ArticleSection'
 import axios from "axios";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
@@ -93,42 +94,7 @@ function OurInstitutions() {
         </div>
       </div>
 
-      <div
-        className="startingContent"
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "5rem",
-          paddingBottom: "2rem",
-        }}
-      >
-        <div
-          className="startingContentContainer"
-          style={{ width: "90%", display: "flex" }}
-        >
-          <div
-            className="startingContentImg"
-            style={{
-              width: "40%",
-              aspectRatio: "1/1",
-              position: "relative",
-              minWidth: "25rem",
-            }}
-          >
-            <div
-              className="backgroundLayer"
-              style={{
-                width: "90%",
-                aspectRatio: "1/1",
-                background: "#F0F0F0",
-                borderRadius: "10%",
-                position: "absolute",
-              }}
-            >
-              <img src={zigzag} className="zigZag1" alt="" />
-              <img src={zigzag} className="zigZag2" alt="" />
-            </div>
+      
 
 
 
@@ -146,7 +112,7 @@ function OurInstitutions() {
                         </div>
 
 
-
+                           
                     </div>
                     <div className='startingContentText' style={{ width: '60%', display: 'flex', alignItems: 'start', justifyContent: 'end',position:'relative' }}>
                         <div style={{ width: '93%' }}>
@@ -160,69 +126,13 @@ function OurInstitutions() {
                             </ul>
                         </div>
                         <img className='zigzagBlue' style={{width:'5rem',top:'0',right:'0',position:'absolute'}} src={zigzagBlue} alt="" />
-
+                      
                     </div>
-
+                                            
                 </div>
+                     
+            </div>
 
-            </div>
-          </div>
-          <div
-            className="startingContentText"
-            style={{
-              width: "60%",
-              display: "flex",
-              alignItems: "start",
-              justifyContent: "end",
-            }}
-          >
-            <div style={{ width: "93%" }}>
-              <h2
-                style={{
-                  marginTop: "0",
-                  fontSize: "clamp(1.5rem,3vw,3rem)",
-                  color: " #303030",
-                  fontWeight: "600",
-                  marginBottom: "10px",
-                }}
-              >
-                Our Institutions
-              </h2>
-              <p
-                style={{
-                  textAlign: "justify",
-                  fontSize: "0.9rem",
-                  lineHeight: "23px",
-                  marginBottom: "0",
-                }}
-              >
-                Westford operates prominent 10 independent brands – 8 in higher
-                education, 1 brand of film studies, and 1 brand in Sports
-                Management.
-              </p>
-              <ul className="institutionList">
-                <li>
-                  The seven higher education entities comprise Acacia
-                  University, Arizona, US, CIQ, UK, Westford University College,
-                  Exeed College, Exeed ECX, Eaton Business School, Athena Global
-                  Education, DeMont Institute of Management & Technology and
-                  Westford Institute of Film Technology with courses ranging
-                  from the undergraduate level to the doctorate level.
-                </li>
-                <li>
-                  Taito Education, a division of Exeed College provides Teacher
-                  education degree programs and teacher training qualifications
-                  from the university and awarding bodies based in the UK.
-                </li>
-                <li>
-                  Westford Sports organizes and conducts sports activities in
-                  connection with school managements and facilities.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div
         class="independent-brands"
