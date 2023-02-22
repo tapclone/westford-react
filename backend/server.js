8cfea9fcf58e842f4619d3585f4dd9da8adfd1a1
Asif <asifsaheer7034@gmail.com>
2023-02-22 18:38:41 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ db.connect((err) => {
 __dirname = path.resolve();
 app.use('/images',express.static(path.join(__dirname,"images")))
 if (NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname,"/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend","build","index.html"));
   });
 }
 
