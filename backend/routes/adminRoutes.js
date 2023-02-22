25d2ac967a3d3e3be7f3e5d3cddce94cdeec3dcc
Asif <asifsaheer7034@gmail.com>
2023-02-22 16:42:28 +0530

    updated

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../middelware/jwtToken");
+// const { verifyToken } = require("../middelware/jwtToken");
 const multer = require("multer");
 const upload = multer({ dest: "images/" });
 const {
